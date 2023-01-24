25af4fb2ba0e017fd7705318492412abdca6e1e5
Isoyor Kubiti <ikubiti@icloud.com>
2023-01-24 00:20:08 -0500

    added Engineer class and tests to the application

---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,93 @@
+const Engineer = require("../lib/engineer");
+
+describe("Engineer", () => {
+	describe("Initialization", () => {
+		it("should create a Engineer object with a name, id, email, and github username if provided valid arguments", () => {
+			const supervisor = new Engineer('Michael', 'michael@something.com', 'imichael');
+
+			expect(supervisor.name).toEqual("Michael");
+			expect(supervisor.id).toEqual(1);
+			expect(supervisor.email).toEqual("michael@something.com");
+			expect(supervisor.github).toEqual('imichael');
+		});
+
+		it("should throw an error if provided no arguments", () => {
+			const staff = () => new Engineer();
+
+			expect(staff).toThrow();
+		});
+
+		it("should throw an error if not provided a Github username", () => {
+			const staff = () => new Engineer("Michael", 'michael@something.com');
+			const err = new Error("Expected parameter 'github' to be a non-empty string");
+
+			expect(staff).toThrowError(err);
+		});
+
+		it("should throw an error if 'email' is not a string", () => {
+			const staff = () => new Engineer('Michael', 25, 'imichael');
+			const err = new Error("Expected parameter 'Email' to be a non-empty string");
+
+			expect(staff).toThrowError(err);
+		});
+
+		it("should throw an error if 'github' username is not a string", () => {
+			const staff = () => new Engineer('Michael', 'michael@something.com', 30);
+			const err = new Error("Expected parameter 'github' to be a non-empty string");
+
+			expect(staff).toThrowError(err);
+		});
+	});
+
+	describe("getEmail", () => {
+		it("should return the email of an employee", () => {
+			// Create new Engineers to perform the tests
+			const supervisor = new Engineer('Michael', 'michael@something.com', 'imichael');
+			const staff1 = new Engineer('Jones', 'jones@something.com', 'jimjones');
+
+			// Verify that the getName() method returns the Engineer's name
+			expect(supervisor.getEmail()).toEqual('michael@something.com');
+			expect(staff1.getEmail()).toEqual('jones@something.com');
+		});
+	});
+
+	describe("getId", () => {
+		it("should return the unique id of a Engineer", () => {
+			// Create new Engineers to perform the tests
+			const supervisor = new Engineer('Michael', 'michael@something.com', 'imichael');
+			const staff1 = new Engineer('Jones', 'jones@something.com', 'jimjones');
+			const staff2 = new Engineer('Ben', 'mben@something.com', 'benjohnson');
+
+			// Verify that the getId() method returns the Engineer's unique id
+			expect(supervisor.getId()).toEqual(6);
+			expect(staff1.getId()).toEqual(7);
+			expect(staff2.getId()).toEqual(8);
+		});
+	});
+
+	describe("getGithub", () => {
+		it("should return the email address of a Engineer", () => {
+			// Create new Engineers to perform the tests
+			const supervisor = new Engineer('Michael', 'michael@something.com', 'imichael');
+			const staff1 = new Engineer('Jones', 'jones@something.com', 'jimjones');
+			const staff2 = new Engineer('Ben', 'mben@something.com', 'benjohnson');
+
+			// Verify that the getGithub() method returns the Engineer's github username
+			expect(supervisor.getGithub()).toEqual('imichael');
+			expect(staff1.getGithub()).toEqual('jimjones');
+			expect(staff2.getGithub()).toEqual('benjohnson');
+		});
+	});
+
+	describe("getRole", () => {
+		it("should return the role of an employee", () => {
+			// Create new Engineers to perform the tests
+			const staff1 = new Engineer('Jones', 'jones@something.com', 'jimjones');
+			const staff2 = new Engineer('Ben', 'mben@something.com', 'benjohnson');
+
+			// Verify that the getRole() method returns "Engineer"
+			expect(staff1.getRole()).toEqual('Engineer');
+			expect(staff2.getRole()).toEqual('Engineer');
+		});
+	});
+});
